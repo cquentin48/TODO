a322db427f861b50209d909c6be06d927c48a4bc
Quentin CHAPEL <chapelquen@gmail.com>
2019-02-14 15:53:32 +0100

    [Feat]Question 17 : Associating new controller to add item view
    Associating created class AddItemViewController to add item view.

---
+++ Checklists/Controllers/AddItemViewController.swift
@@ -0,0 +1,14 @@
+
+//
+//  AddItemViewController.swift--.swift
+//  Checklists
+//
+//  Created by lpiem on 14/02/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import Foundation
+
+class AddItemViewController: UITableViewController {
+    
+}
