1c0781938fcf018fcb8e42f7474877bc3b761170
Quentin CHAPEL <chapelquen@gmail.com>
2019-03-14 17:28:31 +0100

    [Feat]Question 83 : Notification
    Implementing notification permissions when trying to load the application for the first time

--- Checklists/Controllers/AppDelegate.swift
+++ Checklists/Controllers/AppDelegate.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    
+    func checkForNotificationPermission()->Bool{
+        let center = UNUserNotificationCenter.current()
+        var returnValue = false
+        // Request permission to display alerts and play sounds.
+        center.requestAuthorization(options: [.alert, .sound])
+        { (granted, error) in
+            if(!granted){
+                print("Error : "+error.debugDescription)
+                self.displayAlert(title: "Error", message: "In order to use this app, you must enable notification permission. This application will close.", textButton: "Ok")
+                returnValue = false
+            }else{
+                self.displayAlert(title: "Success", message: "With this application you'll be able to manage check list", textButton: "Ok")
+                returnValue = true
+            }
+        }
+        return returnValue
+    }
+    
+    func displayAlert(title:String, message:String, textButton:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: textButton, style: .cancel, handler: nil))
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+        return checkForNotificationPermission()
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
