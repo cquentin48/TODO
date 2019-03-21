d09997ac9ebf0a440995517cf21c12416a155bbe
Quentin CHAPEL <chapelquen@gmail.com>
2019-03-21 10:57:03 +0100

    [Feat]Question 93
    Adding enum UserDefaultsKeys which manage keys for notification.

---
+++ Checklists/Data/UserDefaultsKeys.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaultsKeys.swift
+//  Checklists
+//
+//  Created by lpiem on 21/03/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import Foundation
+
+enum UserDefaultsKeys{
+    static let notTriggeredInStore = "notTriggeredInStore"
+    static let notTriggeredNotInStore = "notTriggeredNotInStore"
+    static let triggeredInStore = "triggeredInStore"
+    static let triggerNotInStore = "triggerNotInStore"
+}
